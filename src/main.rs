6304dfbeaa66b92079eb0fdb9acea3e1281b7d27
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-25 21:45:38 -0500

    Multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#![allow(dead_code)] // Ignora advertencias sobre funciones no utilizadas
 use rand::Rng;
-
+mod quick_sort;
+use quick_sort::quick_sort;
 
 fn main() {
     let init2: usize = 0;
-    let end2: usize = 10;
+    let end2: usize = 1000;
 
     let mut rng = rand::thread_rng();
-    let mut disorder: Vec<i32> = (0..end2 + 1)
+    let mut disorder: Vec<i32> = (0..=end2)
         .map(|_| rng.gen_range(1..=100)) 
         .collect();
 
@@ -17,34 +17,3 @@ fn main() {
     println!("{:?}", disorder);
 }
 
-fn place_pivot(vector: &mut Vec<i32>, mut start:usize , mut end: usize) -> usize{
-
-    while start < end {
-        while vector[end] >= vector[start] && start < end{
-            end = end - 1;
-        }
-        let mut _old: i32 = vector[end]; 
-        vector[end] = vector[start];
-        vector[start] = _old;
-
-        while vector[start] <= vector[end] && start < end{
-            start = start + 1;
-        }
-        let mut _old: i32 = vector[end]; 
-        vector[end] = vector[start];
-        vector[start] = _old;
-    }
-    start
-}
-// remove the mut in the start and the finish, but in the place_pivot is needed
-fn quick_sort(mut vector: &mut Vec<i32>, start:usize , end: usize) {
-    if start < end {
-        let right_pos = place_pivot(&mut vector, start, end);
-        if right_pos != 0 {
-            quick_sort(&mut vector, start, right_pos - 1);
-        } 
-        quick_sort(&mut vector,right_pos + 1, end);
-        
-    }
-
-}
---
+++ src/quick_sort.rs
@@ -0,0 +1,31 @@
+pub fn place_pivot(vector: &mut Vec<i32>, mut start:usize , mut end: usize) -> usize{
+
+    while start < end {
+        while vector[end] >= vector[start] && start < end{
+            end = end - 1;
+        }
+        let mut _old: i32 = vector[end]; 
+        vector[end] = vector[start];
+        vector[start] = _old;
+
+        while vector[start] <= vector[end] && start < end{
+            start = start + 1;
+        }
+        let mut _old: i32 = vector[end]; 
+        vector[end] = vector[start];
+        vector[start] = _old;
+    }
+    start
+}
+// remove the mut in the start and the finish, but in the place_pivot is needed
+pub fn quick_sort(mut vector: &mut Vec<i32>, start:usize , end: usize) {
+    if start < end {
+        let right_pos = place_pivot(&mut vector, start, end);
+        if right_pos != 0 {
+            quick_sort(&mut vector, start, right_pos - 1);
+        } 
+        quick_sort(&mut vector,right_pos + 1, end);
+        
+    }
+
+}
